f496ef6ba3bf263bb535323a5020ad6f104e7f1c
gabrieljs26 <gabrieljs26.12@gmail.com>
2026-09-10 17:55:45 +0200

    Actualiza build de /app sobre los metadatos SEO nuevos de renderer-web.html

--- app/assets/InquilinosPanel-CkBi09gC.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./PropietariosPanel-BLmJEwSA.js";export{t as InquilinosPanel,e as diasAlquiladosEnAnio};
---
+++ app/assets/InquilinosPanel-p7xtiiaI.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./PropietariosPanel-B29aW9ZO.js";export{t as InquilinosPanel,e as diasAlquiladosEnAnio};
