1956fe8aba1324cc56fac8158ef6a09df653cea9
gabrieljs26 <gabrieljs26.12@gmail.com>
2026-09-16 13:08:31 +0200

    Actualiza build de /app: explorador de Drive y carpetas de inquilino via proxy

--- app/assets/InquilinosPanel-5UyLh84E.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./PropietariosPanel-D0cOLppE.js";export{t as InquilinosPanel,e as diasAlquiladosEnAnio};
---
+++ app/assets/InquilinosPanel-DHuwpiga.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./PropietariosPanel-DKsg6MI6.js";export{t as InquilinosPanel,e as diasAlquiladosEnAnio};
