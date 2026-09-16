6ec69d938b63ed633d5542afec6d4ca0befe0862
gabrieljs26 <gabrieljs26.12@gmail.com>
2026-09-16 12:52:07 +0200

    Actualiza build de /app: Drive via proxy en web y sin iframe legacy

---
+++ app/assets/InquilinosPanel-5UyLh84E.js
@@ -0,0 +1,1 @@
+import{n as e,t}from"./PropietariosPanel-D0cOLppE.js";export{t as InquilinosPanel,e as diasAlquiladosEnAnio};
--- app/assets/InquilinosPanel-CdrpQ6kF.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./PropietariosPanel-Dj1LSkVt.js";export{t as InquilinosPanel,e as diasAlquiladosEnAnio};
